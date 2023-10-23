63d4010a359525d64d54910b238e29cb11641a08
Ashish Sahu <ashsahu9713@gmail.com>
2023-10-23 07:53:42 +0530

    Update

--- To Do Web Application/Application/TodosCommands/DeleteTodoCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace To_Do_Web_Application.Application.TodosCommands
-{
-    public class DeleteTodoCommand : IRequest<bool>
-    {
-        public int Id { get; set; }
-    }
-}
--- To Do Web Application/Domain/Models/Todo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace To_Do_Web_Application.Domain.Models
-{
-    public class Todo
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
